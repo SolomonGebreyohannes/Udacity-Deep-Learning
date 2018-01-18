d34ca350c6a45f962e787e1f86dba424006a1281
Solomon Gebreyohannes <solomongebreyohannes@Solomons-MacBook-Pro.local>
2018-01-17 20:22:12 -0500

    Reviewer comments applied

--- first-neural-network/my_answers.py
+++ first-neural-network/my_answers.py
@@ -140,7 +140,7 @@ class NeuralNetwork(object):
 #########################################################
 # Set your hyperparameters here
 ##########################################################
-iterations    = 2000
-learning_rate = 0.2
-hidden_nodes  = 50
-output_nodes  = 10
+iterations    = 5000
+learning_rate = 0.4
+hidden_nodes  = 30
+output_nodes  = 1
